bc516b3471cebd10d49a790447bc19567bf5ada5
skitak <bas2205@live.fr>
2018-02-11 05:19:42 -0500

    backend shop added

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { ItemModel,ItemView,ItemController } from "./export";
-
+import {Shop} from "./export";
 
 function getURLParameter(name:string) {
     return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
 }
+
+var shop = new Shop();
 
 let page = getURLParameter("page");
 switch(page){
@@ -13,7 +15,7 @@ switch(page){
         break;
     default:
         let itemModel = new ItemModel(1,"Test","test.jpg",10.99,"Lorem ipsum");
-        let itemController = new ItemController(itemModel);
+        let itemController = new ItemController(shop.products[0]);
         let itemView = new ItemView(itemController);
         break;
 }
