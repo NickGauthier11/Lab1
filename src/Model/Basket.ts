827cd30c0cf6546fecfd8f7595a187afb8747af8
skitak <bas2205@live.fr>
2018-02-11 14:12:20 -0500

    basket added shop optimized

---
+++ src/Model/Basket.ts
@@ -0,0 +1,40 @@
+import { ItemModel } from '../export';
+import { Shop } from '../export';
+
+export class Basket {
+    products : number[];
+    shop: Shop;
+    constructor(shop:Shop){
+        this.shop = shop;
+        if (!localStorage.basket){
+            this.products = [];
+            this.saveModifications();
+        }
+        else{
+            this.products = (JSON.parse(localStorage.getItem("kart"))).produits;
+        }
+    }
+
+    public addItem (id:number) : void{
+        let index = this.shop.findItemIndexFromId(id);
+        this.products.push(this.shop.products[index].id);
+        this.products.sort();
+        this.saveModifications();
+    }
+
+    public removeItem (id:number) : void{
+        let index = this.products.indexOf(id);
+        if (index != -1)
+            this.products.splice(index);
+        this.saveModifications();
+    }
+
+    private saveModifications() : void{
+        String(this.products);
+        localStorage.setItem("kart", JSON.stringify({produits :this.products}));
+    }
+
+    public getItemFromid(id:number) : ItemModel {
+        return this.shop.products[id];
+    }
+}
